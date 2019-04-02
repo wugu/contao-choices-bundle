9b18481d1dad8551eb551ac58b601ec940ee0626
Thomas Körner <t.koerner@heimrich-hannot.de>
2019-04-02 14:43:11 +0200

    fixed a typo in config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -9,9 +9,9 @@ $GLOBALS['TL_HOOKS']['getAttributesFromDca']['choices'] = ['huh.choices.event_li
  * JS
  */
 if (System::getContainer()->get('huh.utils.container')->isFrontend()) {
-    $GLOBALS['TL_JAVASCRIPT']['huh_components_choices'] = 'assets/Choices/dist/public/assets/scripts/choices.min.js|static';
+    $GLOBALS['TL_JAVASCRIPT']['huh_components_choices'] = 'assets/choices/dist/public/assets/scripts/choices.min.js|static';
     $GLOBALS['TL_JAVASCRIPT']['contao-choices-bundle']  = 'bundles/heimrichhannotcontaochoices/js/contao-choices-bundle.js|static';
-    $GLOBALS['TL_CSS']['huh_components_choices'] = 'assets/Choices/dist/public/assets/styles/choices.min.css|static';
+    $GLOBALS['TL_CSS']['huh_components_choices'] = 'assets/choices/dist/public/assets/styles/choices.min.css|static';
 }
 
 /**
