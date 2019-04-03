41bafd9966644b2f8a0036ca7153e457856839a1
Thomas Körner <t.koerner@heimrich-hannot.de>
2019-04-03 09:28:37 +0200

    v0.5.0

---
+++ src/Resources/contao/languages/en/default.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang = &$GLOBALS['TL_LANG']['MSC'];
+
+/**
+ * Fields
+ */
+$lang['choices.js'] = [
+    'loadingText' => 'Lädt...',
+    'noResultsText' => 'Keine Ergebnisse gefunden',
+    'noChoicesText' => 'Keine Auswahlmöglichkeiten gefunden',
+    'itemSelectText' => 'Zur Auswahl anklicken',
+    'addItemText' => 'Eingabetaste drücken um <b>"${value}"</b> hinzuzufügen',
+    'maxItemText' => 'Es können nur ${maxItemCount} Werte hinzugefügt werden.'
+];
